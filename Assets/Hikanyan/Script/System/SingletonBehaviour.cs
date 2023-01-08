297a39eca3e71dd73196d930d0148eca556aab21
Hikanyan <hikanyan0131@gmail.com>
2023-01-08 20:23:40 +0900

    【Add】SequenceManager

--- Assets/Hikanyan/Script/System/SingletonBehaviour.cs
+++ Assets/Hikanyan/Script/System/SingletonBehaviour.cs
@@ -3,56 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+namespace Hikanyan.Core
 {
-    protected static T _instance;
-
-
-    public static T Instance
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
+        protected static T _instance;
+
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Type t = typeof(T);
+                    _instance = (T)FindObjectOfType(t);
+
+                    if (_instance == null)
+                    {
+                        Debug.LogError($"{t}をアタッチしているGameObjectがありません");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            OnAwake();
+            ChackIns();
+        }
+
+
+        //継承先でAwakeが必要な場合はこれを呼ぶ
+        protected virtual void OnAwake() { }
+
+        protected void ChackIns()
         {
             if (_instance == null)
             {
-                Type t = typeof(T);
-                _instance = (T)FindObjectOfType(t);
-
-                if (_instance == null)
-                {
-                    Debug.LogError($"{t}をアタッチしているGameObjectがありません");
-                }
+                //キャストの意味を理解する。T型にキャストしている
+                _instance = this as T;
             }
-
-            return _instance;
+            else if (Instance == this)
+            {
+                return;
+            }
+            else if (Instance != this)
+            {
+                //すでにあった時は何もせずに消える
+                Destroy(this);
+            }
         }
+
     }
-
-    protected virtual void Awake()
-    {
-        OnAwake();
-        ChackIns();
-    }
-
-
-    //継承先でAwakeが必要な場合はこれを呼ぶ
-    protected virtual void OnAwake() { }
-
-    protected void ChackIns()
-    {
-        if (_instance == null)
-        {
-            //キャストの意味を理解する。T型にキャストしている
-            _instance = this as T;
-        }
-        else if (Instance == this)
-        {
-            return;
-        }
-        else if (Instance != this)
-        {
-            //すでにあった時は何もせずに消える
-            Destroy(this);
-        }
-    }
-
 }
