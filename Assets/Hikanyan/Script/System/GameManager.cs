ead490d6fccdc723a5c87b2dedaa454d51ebd198
Hikanyan <hikanyan0131@gmail.com>
2023-01-18 10:57:40 +0900

    【Update】GameManager

--- Assets/Hikanyan/Script/System/GameManager.cs
+++ Assets/Hikanyan/Script/System/GameManager.cs
@@ -3,24 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
-namespace Hikanyan.Core
+namespace Hikanyan.Runner
 {
     //時間管理
     //オーディオ
     //パーティクル
     //UI
     //スコア
-    //
+    //ステージ
     public class GameManager : SingletonBehaviour<GameManager>
     {
         [SerializeField] 
         AbstractGameEvent _winEvent;
+
         [SerializeField]
         AbstractGameEvent _loseEvent;
 
+        public bool IsRunning => _isPlaying;
+        bool _isPlaying;
+
+        protected override void Awake()
+        {
+            
+        }
 
         void GameStart()
         {
@@ -30,6 +37,14 @@ namespace Hikanyan.Core
         void GameEnd()
         {
             
+        }
+        public void Win()
+        {
+            _winEvent.Reset();
+        }
+        public void Lose()
+        {
+            _loseEvent.Reset();
         }
         
     }
