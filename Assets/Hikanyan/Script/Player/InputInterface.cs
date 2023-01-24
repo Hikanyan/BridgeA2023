8a9fc308a5d2170abc2c72f41efec097243aefb9
Hikanyan <hikanyan0131@gmail.com>
2023-01-24 21:21:52 +0900

    【Update】InputInterface

--- Assets/Hikanyan/Script/Player/InputInterface.cs
+++ Assets/Hikanyan/Script/Player/InputInterface.cs
@@ -1,8 +1,10 @@
-/// <summary>
-/// Playerからブロックを押された時に呼び出されるインターフェイス
-/// </summary>
-interface InputInterface
+namespace Hikanyan.Core
 {
-    public void Press();
+    /// <summary>
+    /// Playerからブロックを押された時に呼び出されるインターフェイス
+    /// </summary>
+    interface InputInterface
+    {
+        public void Press();
     }
 }
