d15aa419c9340dc241c4bc950b5e0c8735e0461e
Hikanyan <hikanyan0131@gmail.com>
2023-01-24 21:23:33 +0900

    【Add】MusicData

---
+++ Assets/Hikanyan/Script/InGame/Data/MusicData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Hikanyan.Core {
+    public class MusicData
+    {
+        public int MusicNumber;
+        public AssetReferenceT<TextAsset> MusicJsonReference;
+        public float MelayTime;
+    }
+}
