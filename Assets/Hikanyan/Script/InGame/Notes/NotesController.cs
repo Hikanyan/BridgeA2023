e4aa002dda506915dc49b6d31ac03eaa14b1e89a
Hikanyan <hikanyan0131@gmail.com>
2023-01-29 11:30:01 +0900

    【Update】OnClickSceneMove

--- Assets/Hikanyan/Script/InGame/Notes/NotesController.cs
+++ Assets/Hikanyan/Script/InGame/Notes/NotesController.cs
@@ -7,15 +7,66 @@ namespace Hikanyan.Core
 {
     public class NotesController : SingletonBehaviour<NotesController>
     {
-        [SerializeField, Tooltip("ノーツスピード")]
-        float _notesSpeed;
+        private float _blockHeight;
+        private float _notesSpeed;
 
-        void NotesMove()
+        public void SetData(float blockHeight, float notesSpeed)
         {
-            for(int i = 0; i < 3; i++)
+            _blockHeight = blockHeight;
+            _notesSpeed = notesSpeed;
+        }
+
+        void Update()
+        {
+            if (GameTimer.Instance.IsRunning)
             {
-                //[TODO]
+                MoveNotes();
+                CheckNotesIsNothing();
             }
+        }
+
+        List<int> _removeNotesByLaneIndex = new();
+
+        void MoveNotes()
+        {
+            int i = 0;
+            for (int blocki = 0; i < 4; blocki++)
+            {
+                foreach (Notes notes in NotesManager.Instance.BlockNotes[blocki])
+                {
+
+                    NotesControl(i, notes);
+                }
+                i++;
+            }
+            if (_removeNotesByLaneIndex.Count > 0)
+            {
+                foreach (int temp in _removeNotesByLaneIndex)
+                {
+                    if (NotesManager.Instance.BlockNotes[temp].Count <= 0) break;
+                    NotesManager.Instance.BlockNotes[temp].RemoveAt(0);
+                }
+                _removeNotesByLaneIndex.Clear();
+            }
+        }
+        void NotesControl(int i, Notes notes)
+        {
+            float judgeTime = GameTimer.Instance.RealTime - notes.Time;
+
+            float pos = _blockHeight - judgeTime / _blockHeight * _blockHeight * _notesSpeed - _blockHeight;
+
+            if (!notes.Visible && pos <= _blockHeight)//表示範囲に入ったときノーツを見えるようにする
+            {
+                notes.SetVisible(true);
+            }
+        }
+        void CheckNotesIsNothing()
+        {
+            foreach (var lane in NotesManager.Instance.BlockNotes)
+            {
+                if (lane.Count > 0) return;
+            }
+            GameManager.Instance.GameEnd();
         }
     }
 }
